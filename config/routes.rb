d9a9deb7fa7046672ab33eb4e9fe9cceaa098657
Lais Almeida <laisdev25@gmail.com>
2024-05-24 18:45:18 -0300

    (UNDONE) Configuração das telas de Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :managers,
+  path: "managers",
+  controllers: {
+    sessions: 'managers/sessions'
+  }
+
+  devise_for :customers,
+    path: "customers",
+    controllers: {
+      sessions: 'customers/sessions'
+    }
+
   root to: "home#index"
+  get "/login", to: "home#login"
 end
